d590025d21706be5edb1f3ea9a4ce5f84dabf045
Griffin Berlstein <griffin@berlste.in>
2021-11-16 23:50:48 -0500

    I hate graphs part one

---
+++ visualize.py
@@ -0,0 +1,136 @@
+import matplotlib.pyplot as plt
+from collections import defaultdict
+import csv
+
+icarus = defaultdict(list)
+verilator = defaultdict(list)
+interp = {}
+lowered = {}
+
+with open("statistics/compilation-results.csv") as file:
+    # compilation,stage,mean,median,stddev
+    for entry in csv.DictReader(file):
+        if entry["stage"] == "icarus-verilog":
+            icarus[entry["compilation"]].append((entry["mean"], entry["stddev"]))
+        elif entry["stage"] == "verilog":
+            verilator[entry["compilation"]].append((entry["mean"], entry["stddev"]))
+
+with open("statistics/simulation-results.csv") as file:
+    for entry in csv.DictReader(file):
+        if entry["stage"] == "interpreter":
+            interp[entry["simulation"]] = (entry["mean"], entry["stddev"])
+        elif entry["stage"] == "icarus-verilog":
+            icarus[entry["simulation"]].append((entry["mean"], entry["stddev"]))
+        elif entry["stage"] == "verilog":
+            verilator[entry["simulation"]].append((entry["mean"], entry["stddev"]))
+
+with open("statistics/simulation-fully-lowered-results.csv") as file:
+    for entry in csv.DictReader(file):
+        lowered[entry["simulation-fully-lowered"]] = (entry["mean"], entry["stddev"])
+
+
+# I have half a functioning brain cell right now, I'm not figuring out how to do this properly
+def remove_linalg(name):
+    if "Linear Algebra" in name:
+        return name[len("Linear Algebra") + 1 :]
+    else:
+        return name
+
+
+bench_order = [x for x in icarus.keys()]
+
+icarus_comp_mean = [float(icarus[bench][0][0]) for bench in bench_order]
+icarus_comp_stddev = [float(icarus[bench][0][1]) for bench in bench_order]
+
+verilator_comp_mean = [float(verilator[bench][0][0]) for bench in bench_order]
+verilator_comp_stddev = [float(verilator[bench][0][1]) for bench in bench_order]
+
+icarus_sim_mean = [float(icarus[bench][1][0]) for bench in bench_order]
+icarus_sim_stddev = [float(icarus[bench][1][1]) for bench in bench_order]
+
+verilator_sim_mean = [float(verilator[bench][1][0]) for bench in bench_order]
+verilator_sim_stddev = [float(verilator[bench][1][1]) for bench in bench_order]
+
+interp_sim_mean = [float(interp[bench][0]) for bench in bench_order]
+interp_sim_stddev = [float(interp[bench][1]) for bench in bench_order]
+
+lowered_sim_mean = [float(lowered[bench][0]) for bench in bench_order]
+lowered_sim_stddev = [float(lowered[bench][1]) for bench in bench_order]
+
+
+width = 0.50
+bench_order = [remove_linalg(x) for x in bench_order]
+
+icarus_labels = [(x) * 3 for x in range(len(bench_order))]
+verilator_labels = [i + width for i in icarus_labels]
+interp_labels = [v + width for v in verilator_labels]
+lowered_labels = [i + width for i in interp_labels]
+
+fig, ax = plt.subplots()
+ax.bar(
+    icarus_labels,
+    icarus_comp_mean,
+    width,
+    yerr=icarus_comp_stddev,
+    tick_label=bench_order,
+    label="Icarus Compilation",
+    log=True,
+)
+ax.bar(
+    verilator_labels,
+    verilator_comp_mean,
+    width,
+    yerr=verilator_comp_stddev,
+    tick_label=bench_order,
+    label="Verilator Compilation",
+    log=True,
+)
+ax.bar(
+    interp_labels,
+    interp_sim_mean,
+    width,
+    yerr=interp_sim_stddev,
+    tick_label=bench_order,
+    label="Interpreter Simulation",
+    log=True,
+)
+ax.bar(
+    lowered_labels,
+    lowered_sim_mean,
+    width,
+    yerr=lowered_sim_stddev,
+    tick_label=bench_order,
+    label="Interpreter (lowered) Simulation",
+    log=True,
+)
+
+
+ax.bar(
+    icarus_labels,
+    icarus_sim_mean,
+    width,
+    yerr=icarus_sim_stddev,
+    bottom=icarus_comp_mean,
+    tick_label=bench_order,
+    label="Icarus Simulation",
+    log=True,
+)
+
+ax.bar(
+    verilator_labels,
+    verilator_sim_mean,
+    width,
+    yerr=verilator_sim_stddev,
+    bottom=verilator_comp_mean,
+    tick_label=bench_order,
+    label="Verilator Simulation",
+    log=True,
+)
+
+plt.xticks(rotation=45)
+ax.set_ylabel("Time in second")
+ax.set_title("Benchmark program")
+ax.legend()
+
+
+plt.show()
