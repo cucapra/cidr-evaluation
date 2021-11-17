0542a14de2dc77fd1c752135eb210e68a81615bf
Chris Gyurgyik <37983775+cgyurgyik@users.noreply.github.com>
2021-11-16 19:13:16 -0800

    Add some juicy messages.

--- scripts/evaluate.sh
+++ scripts/evaluate.sh
@@ -11,27 +11,36 @@
      FILE=$3
 INTERVALS=$4
 
+echo "[RUNNING] interpreter simulation: $PROGRAM"
+
 # Gather Calyx interpreter simulation times.
 for (( i = 0; i < $INTERVALS; ++i ))
 do
     fud e $PROGRAM --to interpreter-out -s verilog.data $DATA \
-    -pr interpreter.interpret -csv \
+    -pr interpreter.interpret -csv -q \
     >> $FILE
 done
+
+echo "[COMPLETE] interpreter simulation: $PROGRAM"
+echo "[RUNNING] iverilog compilation and simulation: $PROGRAM"
 
 # Gather Icarus-Verilog simulation times.
 for (( i = 0; i < $INTERVALS; ++i ))
 do
     fud e $PROGRAM --to dat -s verilog.data $DATA --through icarus-verilog \
-    -pr icarus-verilog.simulate icarus-verilog.compile_with_iverilog -csv \
+    -pr icarus-verilog.simulate icarus-verilog.compile_with_iverilog -csv -q \
     >> $FILE
 done
+
+echo "[COMPLETE] iverilog compilation and simulation: $PROGRAM"
+echo "[RUNNING] verilog compilation and simulation: $PROGRAM"
 
 # Gather Verilog simulation times.
 for (( i = 0; i < $INTERVALS; ++i ))
 do
     fud e $PROGRAM --to dat -s verilog.data $DATA \
-    -pr verilog.simulate verilog.compile_with_verilator -csv \
+    -pr verilog.simulate verilog.compile_with_verilator -csv -q \
     >> $FILE
 done
 
+echo "[COMPLETE] verilog compilation and simulation: $PROGRAM"
