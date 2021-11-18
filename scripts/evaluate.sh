0771fb166626bdad82e2137ba56115c5240ee8da
Griffin Berlstein <griffin@berlste.in>
2021-11-18 14:07:10 -0500

    debugger sanity check

--- scripts/evaluate.sh
+++ scripts/evaluate.sh
@@ -13,10 +13,18 @@ INTERVALS=$4
 
 echo "[RUNNING] interpreter simulation: $PROGRAM"
 
-# Gather Calyx interpreter simulation times.
+# # Gather Calyx interpreter simulation times.
+# for (( i = 0; i < $INTERVALS; ++i ))
+# do
+#     fud e $PROGRAM --to interpreter-out -s verilog.data $DATA \
+#     -pr interpreter.interpret -csv -q \
+#     >> $FILE
+# done
+
+# Gather Calyx debuigger simulation times.
 for (( i = 0; i < $INTERVALS; ++i ))
 do
-    fud e $PROGRAM --to interpreter-out -s verilog.data $DATA \
+    fud e $PROGRAM --to debugger -s verilog.data $DATA -s interpreter.debugger.flags "-p "\
     -pr interpreter.interpret -csv -q \
     >> $FILE
 done
