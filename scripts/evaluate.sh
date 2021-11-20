de4b095b1539f4d058e6a2f1dcd1bc9dfbc8ea42
Griffin Berlstein <griffin@berlste.in>
2021-11-19 21:11:57 -0500

    just icarus trace

--- scripts/evaluate.sh
+++ scripts/evaluate.sh
@@ -22,33 +22,33 @@ echo "[RUNNING] interpreter simulation: $PROGRAM"
 # done
 
 # Gather Calyx debuigger simulation times.
-for (( i = 0; i < $INTERVALS; ++i ))
-do
-    fud e $PROGRAM --to debugger -s verilog.data $DATA -s interpreter.debugger.flags "-p "\
-    -pr interpreter.interpret -csv -q \
-    >> $FILE
-done
+# for (( i = 0; i < $INTERVALS; ++i ))
+# do
+#     fud e $PROGRAM --to debugger -s verilog.data $DATA -s interpreter.debugger.flags "-p "\
+#     -pr interpreter.interpret -csv -q \
+#     >> $FILE
+# done
 
-echo "[COMPLETE] interpreter simulation: $PROGRAM"
+# echo "[COMPLETE] interpreter simulation: $PROGRAM"
 echo "[RUNNING] iverilog compilation and simulation: $PROGRAM"
 
 # Gather Icarus-Verilog simulation times.
 for (( i = 0; i < $INTERVALS; ++i ))
 do
-    fud e $PROGRAM --to dat -s verilog.data $DATA --through icarus-verilog -s futil.flags "-x tdcc:no-early-transitions" \
+    fud e $PROGRAM --to vcd -s verilog.data $DATA --through icarus-verilog -s futil.flags "-x tdcc:no-early-transitions" \
     -pr icarus-verilog.simulate icarus-verilog.compile_with_iverilog -csv -q \
     >> $FILE
 done
 
 echo "[COMPLETE] iverilog compilation and simulation: $PROGRAM"
-echo "[RUNNING] verilog compilation and simulation: $PROGRAM"
+# echo "[RUNNING] verilog compilation and simulation: $PROGRAM"
 
 # Gather Verilog simulation times.
-for (( i = 0; i < $INTERVALS; ++i ))
-do
-    fud e $PROGRAM --to vcd -s verilog.data $DATA -s verilog.vcd-target "/dev/null" -s futil.flags "-x tdcc:no-early-transitions" \
-    -pr verilog.simulate verilog.compile_with_verilator -csv -q \
-    >> $FILE
-done
+# for (( i = 0; i < $INTERVALS; ++i ))
+# do
+#     fud e $PROGRAM --to vcd -s verilog.data $DATA -s verilog.vcd-target "/dev/null" -s futil.flags "-x tdcc:no-early-transitions" \
+#     -pr verilog.simulate verilog.compile_with_verilator -csv -q \
+#     >> $FILE
+# done
 
-echo "[COMPLETE] verilog compilation and simulation: $PROGRAM"
+# echo "[COMPLETE] verilog compilation and simulation: $PROGRAM"
